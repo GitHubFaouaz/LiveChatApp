8ee49b66cdccd8fdf0cd61577c16f6f5c6e022e9
GitHubFaouaz <faouaza@yahoo.com>
2024-07-22 01:22:59 +0200

    nav

--- my-app/app/components/Nav.tsx
+++ my-app/app/components/Nav.tsx
@@ -1,4 +1,19 @@
 // rfc
+"use client";
+
+import Link from "next/link";
+import { FaHome, FaUser } from "react-icons/fa";
+
 export default function Nav() {
-  return <div>Nav</div>;
+  return (
+    <nav className="h-[70px] w-full flex justify-between items-center bg-blue-600 p-3">
+      <Link href="/">
+        <li className="text-white flex items-center gap-2 p-2 rounded-full hover:text-blue-600 hover:bg-white transition-all">
+          <FaHome />
+          <span>Home</span>
+        </li>
+      </Link>
+    </nav>
+  );
 }
+// li.text-white.flex.items-center.gap-2.rounded-full.hover:text-blue-600.hover:bg-white.transition-all un raccourci
