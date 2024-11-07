7043613939f0d56a4c1cc02aff68c6f0092e0aef
GitHubFaouaz <faouaza@yahoo.com>
2024-11-07 17:01:48 +0100

    bd

--- app/composant/Nav.tsx
+++ app/composant/Nav.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { FaHome, FaUser } from "react-icons/fa";
 
 export default function Nav() {
-  return <div>Nav</div>;
+  const router = useRouter();
+  const goToDashboard = () => {
+    router.push("/dashboard");
+  };
+  return (
+    <nav className="h-[70px] w-full flex justify-between items-center bg-blue-600 p-3">
+      <Link href="/">
+        <li className="text-white flex items-center gap-2 p-2 rounded-full hover:text-blue-600 hover:bg-white transition-all">
+          <FaHome />
+          <span>Home</span>
+        </li>
+      </Link>
+      <button
+        onClick={goToDashboard}
+        className="text-white flex items-center gap-2 p-2 rounded-full hover:text-blue-600 hover:bg-white transition-all"
+      >
+        <FaUser />
+      </button>
+    </nav>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,35 @@
-import Image from "next/image";
+import { FcGoogle } from "react-icons/fc";
+import Nav from "./composant/Nav";
 
 export default function Home() {
-  return <div>holla</div>;
+  return (
+    <div>
+      <Nav />
+      <section className="w-full h-screen flex items-center justify-center p-3">
+        <div className="max-w-[1000px] m-auto flex items-center justify-center flex-col gap-5 text-center">
+          <h1 className="text-6xl uppercase font-black ">
+            CHAT <span className="text-blue-600">APP</span>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consectetur, esse hic velit quis quas, molestias voluptatem non
+            voluptatum, earum eos alias accusamus in voluptatibus repellendus
+            provident? Ipsam, fugiat dolore iusto nesciunt illum quisquam ex
+            illo facilis consectetur porro eligendi in at? Atque pariatur quasi
+            vitae voluptatibus quidem praesentium officia quia quis labore nihil
+            dicta natus nostrum dolorem est, minima consectetur asperiores
+            voluptas eligendi ut, rerum explicabo. Reprehenderit, sequi modi
+            laborum amet illum inventore unde earum neque repellat sed maxime?
+            Porro provident vel architecto perspiciatis quas maiores dolores
+            consequatur aut at sed, repellendus, dolor tempore, veritatis quae
+            nostrum? In, voluptates pariatur, dolorum sapiente, error nulla ut
+            reprehenderit recusandae quasi fuga rerum autem sunt. Veritatis !
+          </p>
+          <button className="bg-gray-200 hover:bg-gray-300 rounded-md p-2 flex items-center gap-2 ">
+            <FcGoogle /> Se conecter avec google
+          </button>
+        </div>
+      </section>
+    </div>
+  );
 }
