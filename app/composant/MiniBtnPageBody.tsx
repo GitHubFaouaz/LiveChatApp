6d640986be53b76ad403f46c32d83f196997382b
GitHubFaouaz <faouaza@yahoo.com>
2025-01-29 11:51:07 +0100

    components minibtn page homme

--- app/composant/MiniBtnPageBody.tsx
+++ app/composant/MiniBtnPageBody.tsx
@@ -1,11 +1,33 @@
 import React from "react";
-
+import "./_MiniBtnPageBody.scss";
 export default function MiniBtnPageBody() {
   return (
     <div className="absolute z-[2] w-[100%] h-[100%] ">
-      <span className="absolute top-[250px] left-[10px] text-[#000]  bg-red-200 rounded-3xl w-[25px] h-[25px]"></span>
-      <span className="absolute top-[100px] right-[200px] text-[#000]  bg-red-200 rounded-3xl w-[25px] h-[25px]"></span>
-      <span className="absolute bottom-[150px] left-[200px] text-[#000]  bg-red-200 rounded-3xl w-[25px] h-[25px]"></span>
+      <div
+        className="absolute top-[250px] left-[10px] text-[#000]  rounded-3xl w-[25px] h-[25px] "
+        id="cube"
+      >
+        <div className="side" id="front"></div>
+        <div className="side" id="bottom"></div>
+        <div className="side" id="top"></div>
+        <div className="side" id="left"></div>
+        <div className="side" id="right"></div>
+        <div className="side" id="back"></div>
+      </div>
+
+      <div
+        className="absolute top-[100px] right-[20px] text-[#000]  rounded-3xl w-[25px] h-[25px]"
+        id="cube1"
+      >
+        <div className="side" id="front"></div>
+        {/* <div className="side" id="bottom"></div> */}
+        {/* <div className="side" id="top"></div> */}
+        <div className="side" id="left"></div>
+        <div className="side" id="right"></div>
+        <div className="side" id="back"></div>
+      </div>
+
+      <div className="absolute bottom-[150px] left-[200px] text-[#000]  bg-red-200 rounded-3xl w-[25px] h-[25px]"></div>
     </div>
   );
 }
