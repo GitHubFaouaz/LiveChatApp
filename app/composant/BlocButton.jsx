1564062ac91dfc45e5d850a5f6f4c556caeb7c68
GitHubFaouaz <faouaza@yahoo.com>
2025-02-16 21:20:44 +0100

    PHOTO PROFIL

--- app/composant/BlocButton.jsx
+++ app/composant/BlocButton.jsx
@@ -4,7 +4,7 @@ export default function blocButton() {
   return (
     <div className="flex flex-col ">
       <div className=" w-[100%] bg-red-200 rounded-[15px] p-[15px]">
-        <span className="w-[100%] h-[5px] bg-[#000] flex"></span>
+        <span className="w-[100%] h-[10px] bg-[#000] flex before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-pink-500"></span>
       </div>
       <div></div>
       <div></div>
