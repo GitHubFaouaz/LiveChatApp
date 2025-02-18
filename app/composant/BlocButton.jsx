bec3f82803c086cb62dd687af83fb29001b12580
GitHubFaouaz <faouaza@yahoo.com>
2025-02-18 16:57:10 +0100

    PHOTO PROFIL

--- app/composant/BlocButton.jsx
+++ app/composant/BlocButton.jsx
@@ -3,7 +3,7 @@ import "./blocButton.scss";
 export default function blocButton() {
   return (
     <div className=" flex flex-col " id="containeBlocButton">
-      <div className=" w-[100%] rounded-[15px] p-[15px] ">
+      <div className=" w-[100%] rounded-[15px] p-[15px] border-4 border-[#000]  ">
         <span className="w-[100%]  bg-[#000] p-[5px] flex  spanBarre  "></span>
       </div>
       <div></div>
